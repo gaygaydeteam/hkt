9ba133730ead3a57c6694f97adb83387260ec977
Garry Tan <li4zhendepi@gmail.com>
2019-02-21 13:56:43 +0800

    rm

--- src/Pages/My.js
+++ src/Pages/My.js
@@ -9,8 +9,8 @@ const styles = {
             marginTop: theme.appTopHeight,
         },
         avatar: {
-            width: 80,
-            height: 80,
+            width: 55,
+            height: 55,
             borderRadius: 40,
             marginLeft: 15.
         },
@@ -92,9 +92,9 @@ export default class My extends Component {
 				<ImageBackground source={appBg} style={styles.backgroundImage}>
                     <UserProfile editable={true} style={styles.userProfile}/>
                     <ScrollView>
-                        <List name="个人信息" icon="vcard-o" id="PersonalInfo" nav={navigation}/>
-                        <List name="收获地址" icon="tags" nav={navigation}/>
+                        <List name="实名认证" icon="vcard-o" id="PersonalInfo" nav={navigation}/>
                         <List name="修改密码" icon="cart-plus" id="ChangePassword" nav={navigation}/>
+                        <List name="收获地址" icon="tags" nav={navigation}/>
                     </ScrollView>
 				</ImageBackground>
 			</View>
